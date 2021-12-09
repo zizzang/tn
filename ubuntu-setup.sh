82f63391cc4701939ada0c4ef8180051782a6dea
zizzang <90598260+zizzang@users.noreply.github.com>
2021-12-09 16:46:44 +0900

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
+# setting
 
-# cd
-cd /home/ansibleadmin
-
-# update
 sudo apt-get update
-sudo apt upgrade -y
+sudo apt-get upgrade -y
 sudo apt autoremove --assume-yes
-sudo apt install python3-pip
-
-# install ansible
+sudo apt install python3-pip -y
 pip3 install --upgrade pip
 pip3 install "ansible==2.9.17"
+pip3 install ansible[azure]
 pip3 install ansible[azure]
